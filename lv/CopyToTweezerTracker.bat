97a66ab3afd2ecfa62c50f8bf6bcf0dc34c99ccb
jelmer <jelmer@jelmer.com>
2013-08-09 14:35:24 +0200

    * rename binaries jtrk->qtrk

---
+++ lv/CopyToTweezerTracker.bat
@@ -0,0 +1,8 @@
+set DSTPATH=..\..\bin\qtrk
+copy /Y cudart32_50_35.dll %DSTPATH%
+copy /Y qtrkcudad-labview.dll %DSTPATH%
+copy /Y qtrkcuda-labview.dll %DSTPATH%
+copy /Y qtrk-labview.dll %DSTPATH%
+copy /Y qtrkd-labview.dll %DSTPATH%
+copy /Y QTrk.llb %DSTPATH%
+pause
