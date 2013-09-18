03959426f25aa460f6b3d4d373c7949a14055a0b
Jelmer <j.cnossen@gmail.com>
2013-09-18 04:04:04 +0200

    * RPTree class

--- ldmanifoldtrk/main.cpp
+++ ldmanifoldtrk/main.cpp
@@ -2,6 +2,7 @@
 #include "std_incl.h"
 #include "utils.h"
 #include "tinydir.h"
+#include <list>
 
 typedef void (*process_image_cb)(ImageData *img, int bead, int frame);
 
@@ -50,14 +51,11 @@ void extract_regions(std::vector<BeadPos> beads, int size, int frame, ImageData*
 	roi.free();
 }
 
-void process_beads(const char *path, int size, process_image_cb cb)
+void process_beads(const char *path, int size, std::vector<BeadPos> beadlist, process_image_cb cb, int framelimit)
 {
 	tinydir_dir d;
 	if (tinydir_open(&d, path) == -1)
 		throw std::runtime_error("can't open given path");
-
-	auto beadlist = read_beadlist(std::string(path) + "/beadlist.txt");
-	dbgprintf("%d beads.\n", beadlist.size());
 
 	int frame=0;
 	while(d.has_next) {
@@ -72,18 +70,57 @@ void process_beads(const char *path, int size, process_image_cb cb)
 			img.free();
 		}
 
+		if (frame==framelimit)
+			break;
+
 		tinydir_next(&d);
 	}
 }
 
+struct RPTree {
+	int D;
+	std::list<float*> points;
+
+	int memuse() { return points.size()*D*sizeof(float); }
+	RPTree(int D) : D(D) {}
+	~RPTree() {	DeleteAllElems(points); }
+
+	void add(float* pt) {
+		float* cp = new float[D];
+		for (int i=0;i<D;i++) cp[i]=pt[i];
+		points.push_back(cp);
+	}
+};
+
+std::vector<RPTree*> trees;
+
 void build_tree(ImageData* img, int bead, int frame)
 {
+	trees[bead]->add(img->data);
+}
 
+void print_memuse()
+{
+	int memuse=0;
+	for (int i=0;i<trees.size();i++)
+		memuse+=trees[i]->memuse();
+	dbgprintf("Memuse: %d\n", memuse);
 }
 
 int main(int argc, char* argv[])
 {
-	process_beads("../../datasets/1/tmp_001", 80, build_tree);
+	const char *path = "../../datasets/1/tmp_001";
+	auto beadlist = read_beadlist(std::string(path) + "/beadlist.txt");
+	dbgprintf("%d beads.\n", beadlist.size());
 
+	int W=80;
+	for (int i=0;i<beadlist.size();i++)
+		trees.push_back(new RPTree(W*W));
+	process_beads(path, W, beadlist, build_tree, 40);
+
+	print_memuse();
+
+	DeleteAllElems(trees);
 	return 0;
 }
+
