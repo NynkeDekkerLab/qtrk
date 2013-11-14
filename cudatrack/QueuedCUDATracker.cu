fc50c3566745e6cd209b43ee03f285f96a5d1aa6
Jelmer <Jelmer>
2013-11-14 17:46:29 +0100

    Benchmark QI radial steps

--- cudatrack/QueuedCUDATracker.cu
+++ cudatrack/QueuedCUDATracker.cu
@@ -170,7 +170,7 @@ QueuedCUDATracker::QueuedCUDATracker(const QTrkComputedConfig& cc, int batchSize
 	}
 	this->batchSize = batchSize;
 
-	dbgprintf("# of CUDA processors:%d. Using %d streams\n", deviceProp.multiProcessorCount, numStreams);
+	dbgprintf("CUDA Hardware: %s. \n# of CUDA processors:%d. Using %d streams\n", deviceProp.name, deviceProp.multiProcessorCount, numStreams);
 	dbgprintf("Warp size: %d. Max threads: %d, Batch size: %d\n", deviceProp.warpSize, deviceProp.maxThreadsPerBlock, batchSize);
 	
 	qi.Init(cfg, batchSize);
