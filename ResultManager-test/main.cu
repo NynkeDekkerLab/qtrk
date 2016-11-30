2cdc24cc52fbf2743cb4302b0ef1db2e2af31c0f
Josko de Boer (Daimonie) <joskodeboer@gmail.com>
2016-11-30 16:28:53 +0100

    Added comments. Note that the test is now set to 5 QI Iterations, making it more comparable with Jordi's setup overview.

--- ResultManager-test/main.cu
+++ ResultManager-test/main.cu
@@ -32,28 +32,29 @@ This file tests the Result Manager. It is pretty straightforward:
 */
 int main(int argc, char* argv[])
 {
+	//At the end, we report the time taken.
 	auto timeStart = boost::chrono::high_resolution_clock::now();
+	//Just a reminder.
 	fprintf(stderr, "Note: Initialising ndlab/test/ResultManager (%d arguments).\n", argc);
-
+	//Otherwise, it will throw errors.
 	if (argc != 3)
 	{
 		fprintf(stderr, "You have to give in N, the number of images (multiple of 4) and \n\t gpu or cpu.\n");
 		return 0;
 	}
-	//number of images/frames
+	//Number of images, which are inputted as if there are 4 beads (which really doesn't matter).
 	const int N = (int)atoi(argv[1]); //4 beads, 24 images; 6 frames per image?
-
+	//Always report back.
 	fprintf(stderr, "Testing ResultManager with %d images. %s \n", N, argv[1]);
-
+	//Where to safe data/frameinfo. 
 	const char* file = "./ResultManagerData.txt";
 	const char * frameinfo = "./ResultManagerFrameInfo.txt";
-	//shared_ptrs are far superior in terms of memory management etc.
-	// make_shared is exception safe.
+	// Better to use modern smart pointers. Config settings are largely taken from older tests.
 	std::shared_ptr<ResultManagerConfig> cfg = std::make_shared<ResultManagerConfig>();
 	cfg->numBeads = 4;
 	cfg->numFrameInfoColumns = 0;
-	cfg->scaling = vector3f(1.0, 1.0, 1.0);
-	cfg->offset = vector3f(0.0, 0.0, 0.0);
+	cfg->scaling = vector3f(1.0f, 1.0f, 1.0f);
+	cfg->offset = vector3f(0.0f, 0.0f, 0.0f);
 	cfg->writeInterval = 2500;
 	cfg->maxFramesInMemory = 0;// 100000;
 	cfg->binaryOutput = false;
@@ -63,18 +64,18 @@ int main(int argc, char* argv[])
 	colNames.push_back(testName);
 
 	fprintf(stderr, "Allocating Result Manager now. \n");
-	// The GodFather manages your results.
+	// The manager is an instance of the Result Manager class. Surprise.
 	std::shared_ptr<ResultManager> manager = std::make_shared<ResultManager>(file, frameinfo, cfg.get(), colNames);
 	//The QueuedCPUTracker instance is required to retrieve the results. It needs settings.
 	QTrkComputedConfig settings;
-	settings.qi_iterations = 2;
+	settings.qi_iterations = 5;
 	settings.zlut_minradius = 1;
 	settings.qi_minradius = 1;
 	settings.width = settings.height = 100;
 	settings.Update();
-
+	//Experimental file to use for test.
 	std::string fileName = "./exp.jpg";
-
+	//Make sure the file exists; otherwise, it will throw a really weird error.
 	bool fileExists = boost::filesystem::exists(fileName);
 
 	if (!fileExists)
@@ -87,7 +88,7 @@ int main(int argc, char* argv[])
 	auto data = ReadJPEGFile(fileName.c_str());
 
 	std::shared_ptr<QueuedTracker> qtrk;
-
+	//Make sure we are using the right tracker (as requested)
 	if (argc == 3)
 	{
 		std::string argTracker = std::string(argv[2]);
@@ -120,7 +121,7 @@ int main(int argc, char* argv[])
 
 	qtrk->SetLocalizationMode(modeQI);
 
-
+	//Make the calculations
 	std::vector<LocalizationJob> jobs;
 
 	int frame = 0;
@@ -132,13 +133,14 @@ int main(int argc, char* argv[])
 		}
 		//Make a localization job (batch of calculations)
 		LocalizationJob job(frame, 0, 0, 0);
-		job.zlutIndex = i % 4;
+		job.zlutIndex = i % 4; //actually, frame number
 		jobs.push_back(job);
 		qtrk->ScheduleImageData(&data, &job);
 	}
 	manager->SetTracker(qtrk.get());
 	//Process images (using Flush because Start is CPU only)
 	qtrk->Flush();
+	//Report progress
 	int i = 0;
 	while (manager->GetFrameCounters().localizationsDone < N)
 	{
@@ -150,9 +152,7 @@ int main(int argc, char* argv[])
 		}
 		i++;
 	}
-
-	//Assign tracker
-
+	 
 	//Assign frame
 	float somefloat = 0.0;
 	for (int i = 0; i < N / 4; i++)
@@ -164,7 +164,7 @@ int main(int argc, char* argv[])
 
 	vector3f startPosition(0.0f, 0.0f, 0.0f);
 	vector2f initialGuess(45.0f, 50.0f);
-
+	//Allocate the 'array'  that will hold the results.
 	for (int i = 0; i < N; i++)
 	{
 		LocalizationResult currentResult;
@@ -190,9 +190,10 @@ int main(int argc, char* argv[])
 			fprintf(stderr, "Update[%.3f]: %d frames saved.\n", i, counters.lastSaveFrame);
 		}
 	}
-
+	//Get the results
 	auto counters = manager->GetFrameCounters();
 	auto getResults = manager->GetResults(results.data(), 0, N / cfg->numBeads);
+	//Report results
 	fprintf(stderr, "ResultManager results (%d) :\n", getResults);
 	fprintf(stderr, "\t frame\t bead\t x\t y\t z\n");
 	for (unsigned int i = (results.size() - 25)>0 ? (results.size() - 25) : 0; i < results.size(); i++)
